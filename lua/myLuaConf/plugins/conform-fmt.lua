5fd9a9ce1e9537fbb9d97ea601b83f54e2055e5e
Blaz Prah <blaz@echo.si>
2024-01-09 11:09:30 +0100

    add format on save for nix with alejandra

--- lua/myLuaConf/plugins/conform-fmt.lua
+++ lua/myLuaConf/plugins/conform-fmt.lua
@@ -1,12 +1,12 @@
 require("conform").setup({
   formatters_by_ft = {
-    nix = { "nixfmt" },
+    nix = { "alejandra" },
     -- csharp = { "uncrustify" },
   },
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 })
 
