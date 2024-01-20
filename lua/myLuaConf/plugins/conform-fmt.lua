ab224b8fee43337f94a27e4e0da8cb26312e75bf
Blaz Prah <blaz@echo.si>
2024-01-20 15:53:35 +0100

    change Format command

--- lua/myLuaConf/plugins/conform-fmt.lua
+++ lua/myLuaConf/plugins/conform-fmt.lua
@@ -31,15 +31,31 @@ end, {
   desc = "Re-enable autoformat-on-save",
 })
 
-vim.api.nvim_create_user_command("Format", function(args)
-  local range = nil
-  if args.count ~= -1 then
-    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-    range = {
-      start = { args.line1, 0 },
-      ["end"] = { args.line2, end_line:len() },
+vim.api.nvim_create_user_command('DiffFormat', function()
+  local lines = vim.fn.system('git diff --unified=0'):gmatch('[^\n\r]+')
+  local ranges = {}
+  for line in lines do
+    if line:find('^@@') then
+      local line_nums = line:match('%+.- ')
+      if line_nums:find(',') then
+        local _, _, first, second = line_nums:find('(%d+),(%d+)')
+        table.insert(ranges, {
+          start = { tonumber(first), 0 },
+          ['end'] = { tonumber(first) + tonumber(second), 0 },
+        })
+      else
+        local first = tonumber(line_nums:match('%d+'))
+        table.insert(ranges, {
+          start = { first, 0 },
+          ['end'] = { first + 1, 0 },
+        })
+      end
+    end
+  end
+  local format = require('conform').format
+  for _, range in pairs(ranges) do
+    format {
+      range = range,
     }
   end
-  require("conform").format({ async = true, lsp_fallback = true, range = range })
-end, { range = true })
-
+end, { desc = 'Format changed lines' })
