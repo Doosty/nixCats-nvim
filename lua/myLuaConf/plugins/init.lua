bf51a98a1df5822a2eb4a1e777d7a93b22cc7a95
Blaz Prah <blaz@echo.si>
2024-01-20 14:51:36 +0100

    add eyeliner options

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -111,3 +111,10 @@ local osc52 = require('osc52')
 vim.keymap.set('n', '<leader>y', osc52.copy_operator, {expr = true})
 vim.keymap.set('n', '<leader>yy', '<leader>y_', {remap = true})
 vim.keymap.set('v', '<leader>y', osc52.copy_visual)
+
+-- Eyeliner
+require'eyeliner'.setup {
+  highlight_on_key = true,
+  dim = false
+}
+
